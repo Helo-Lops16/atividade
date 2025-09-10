d9fb391a58634b3e87e5a4ca2fae8b1758111f96
Helo-Lops16 <heloisalopes560@gmail.com>
2025-09-10 14:14:20 -0300

    funcionouuu

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,22 +3,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="text-center max-w-3xl">
-        
-        
-        <div className="ml-[300px] max-w-md mx-auto p-6"> 
-            <h1 className="text-2xl font-bold text-green-900 mb-4">
-            Sobre a Univile
-            </h1>
-            <p className=" ml-[20px] text-green-800 mb-6 leading-relaxed">
-                Somos uma Universidade referência na região, com ampla 
-                estrutura, professores qualificados e cursos reconhecidos pelo MEC, 
-                proporcionamos atividades de ensino, pesquisa e extensão 
-                nas mais diversas áreas do conhecimento.
-            </p>  
+    <main className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="flex flex-col md:flex-row items-center gap-10 max-w-5xl">
+        {/* Imagem */}
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/imagem/sobre.png"
+            alt="Universidade Univille"
+            width={500}
+            height={400}
+            className="rounded-lg shadow-lg w-full h-auto"
+          />
         </div>
-        
+
+        {/* Texto */}
+        <div className="w-full md:w-1/2 text-center md:text-left">
+          <h1 className="text-3xl font-bold text-green-900 mb-4">
+            Sobre a Univille
+          </h1>
+          <p className="text-green-800 leading-relaxed text-5x1">
+            Somos uma Universidade referência na região, com ampla 
+            estrutura, professores qualificados e cursos reconhecidos pelo MEC. 
+            Proporcionamos atividades de ensino, pesquisa e extensão 
+            nas mais diversas áreas do conhecimento.
+          </p>
+        </div>
       </div>
     </main>
   );
