89b568f7be84f25fd94af5fc06376d20f6a9cdb0
Helo-Lops16 <heloisalopes560@gmail.com>
2025-09-10 14:49:51 -0300

    mais ou menos em

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white px-4 mt-20">
       <div className="flex flex-col md:flex-row items-center gap-10 max-w-5xl">
-        {/* Imagem */}
-        <div className="w-full md:w-1/2">
+      
+        <div className="w-full md:w-1/2 mt-10px ">
           <Image
             src="/imagem/sobre.png"
             alt="Universidade Univille"
             width={500}
-            height={400}
+            height={500}
             className="rounded-lg shadow-lg w-full h-auto"
           />
       </div>
@@ -34,11 +34,74 @@ export default function Home() {
         
       </div>
      
-    <div className="mt-[120px] w-full bg-gray-300 py-8 text 3-l ">
-        <h2 className="ml-[30px] text-4xl text-green-700 font-semibold font-saira">
-          Conheça nossos cursos
-        </h2>
-    </div>
+            <section className="bg-gray-200 py-16 px-6 mt-20">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-start gap-10">
+        
+        
+        <div className="md:w-1/3">
+          <h2 className="text-3xl font-bold text-green-900 uppercase mb-4">
+            Conheça<br />nossos<br />cursos
+          </h2>
+          <p className="text-green-800 mb-6">Um universo de oportunidades.</p>
+          <button className="flex items-center gap-2 text-green-800 font-bold underline hover:text-green-600 transition text-1x1">
+            Confira
+            <span className="text-2xl">→</span>
+          </button>
+        </div>
+
+        <div className="md:w-2/3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 h-100">
+        
+          <div className="relative rounded-lg overflow-hidden shadow-lg">
+            <Image
+              src="/imagem/publi.jpg"
+              alt="Curso de Publicidade"
+              width={400}
+              height={300}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute bottom-0 left-0 right-0  bg-opacity-0 text-white text-center py-2">
+              Publicidade
+            </div>
+          </div>
+
+          <div className="relative rounded-lg overflow-hidden shadow-lg h-100">
+            <Image
+              src="/imagem/gastro.jpg"
+              alt="Curso de Gastronomia"
+              width={400}
+              height={300}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute bottom-0 left-0 right-0  text-white text-center py-2">
+              Gastronomia
+            </div>
+          </div>
+
+          <div className="relative rounded-lg overflow-hidden shadow-lg h-100">
+            <Image
+              src="/imagem/med.jpg"
+              alt="Curso de Medicina"
+              width={400}
+              height={300}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute bottom-0 left-0 right-0  text-white text-center py-2">
+              Medicina
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <section className=" text-white py-16 px-6 w-full mt-20"> 
+         <h1 className="text-3xl font-bold text-green-900 mb-4">
+          O que dizem nossos alunos
+        </h1>
+        
+    </section>   
 
 
 
